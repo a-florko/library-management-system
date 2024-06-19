46c44b395a29f4846d1ff0861811d2aa8d4f44d8
ArtemFlorko <artom.florko@gmail.com>
2024-06-19 19:48:37 +0300

    Logic for receiving books and displaying all components of the home page

--- Client/library-management-frontend/src/components/screens/home/Home.tsx
+++ Client/library-management-frontend/src/components/screens/home/Home.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BookList from "./BookList";
+import AddBookModal from "../../ui/AddBookModal";
+import { Book } from "../../../types/BookProps";
+import axios from "axios";
 import TopPanel from "../../ui/TopPanel";
 
-const handleAddBook = () => {
-    console.log("Clicked on add book")
-}
-
 const Home: React.FC = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [books, setBooks] = useState<Book[] | null>(null);
+    const [serverError, setServerError] = useState<boolean>(false);
+
+    useEffect(() => {
+        axios.get<Book[]>('https://localhost:7233/api/books')
+            .then(response => {
+                setBooks(response.data);
+            })
+            .catch(error => {
+                if (error.code === "ERR_NETWORK") {
+                    setServerError(true);
+                }
+                console.error('Error fetching books:', error);
+            });
+    }, []);
+    
+    const toggleModal = () => setShowModal(!showModal);
+
     return (
-        <div>
-            <TopPanel
-                onAddBook={handleAddBook}
-            />
-            <BookList />
-        </div>
+        <>
+            {!serverError && books && (
+                <TopPanel toggleModal={toggleModal}></TopPanel>
+            )}
+            <BookList books={books} serverError={serverError} />
+            <AddBookModal showModal={showModal} toggleModal={toggleModal}/>
+        </>
     )
 }
 
