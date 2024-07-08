8e5b5b6f54551a8279ec7e7337c19be667090731
ArtemFlorko <artom.florko@gmail.com>
2024-07-08 20:23:44 +0300

    Librarian class, Log In DTO and changed book class on backend

--- Server/LibraryManagementAPI/LibraryManagementAPI/Models/Book.cs
+++ Server/LibraryManagementAPI/LibraryManagementAPI/Models/Book.cs
@@ -4,15 +4,15 @@ public partial class Book
 {
     public uint Id { get; set; }
 
-    public string? Title { get; set; }
+    public required string Title { get; set; }
 
-    public string? Author { get; set; }
+    public required string Author { get; set; }
 
     public string? Overview { get; set; }
 
-    public string? Language { get; set; }
+    public required string Language { get; set; }
 
-    public uint? CopiesInStock { get; set; }
+    public uint CopiesInStock { get; set; }
 
-    public uint? TotalCopies { get; set; }
+    public uint TotalCopies { get; set; }
 }
---
+++ Server/LibraryManagementAPI/LibraryManagementAPI/Models/Librarian.cs
@@ -0,0 +1,14 @@
+﻿namespace LibraryManagementAPI.Models;
+
+public partial class Librarian
+{
+    public uint Id { get; set; }
+
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+
+    public required string Login { get; set; }
+
+    public required string Password { get; set; }
+}
