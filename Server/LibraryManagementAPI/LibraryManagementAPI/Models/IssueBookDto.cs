3dd01a880e19abdb25ab0333e9f5b1f1b60d0600
ArtemFlorko <artom.florko@gmail.com>
2024-07-16 00:35:53 +0300

    Backend Issue Book DTO

---
+++ Server/LibraryManagementAPI/LibraryManagementAPI/Models/IssueBookDto.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagementAPI.Models
+{
+    public partial class IssueBookDto
+    {
+        public required string BookTitle { get; set; }
+        public required string FullReaderName { get; set; }
+        public required string IssuedBy { get; set; }
+        public required DateOnly IssueDate { get; set; }
+        public required DateOnly ReturnBefore { get; set; }
+        public string Notes { get; set; } = "";
+    }
+}
