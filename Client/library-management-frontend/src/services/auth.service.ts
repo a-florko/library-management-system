290577389ddba60a3c76ec421f320aa59385be24
ArtemFlorko <artom.florko@gmail.com>
2024-08-01 20:18:24 +0300

    Added getting id during authorisation and displaying potential errors in the console

--- Client/library-management-frontend/src/services/auth.service.ts
+++ Client/library-management-frontend/src/services/auth.service.ts
@@ -3,11 +3,12 @@ import axios from "axios";
 const API_URL = `${process.env.REACT_APP_API_URL}/librarians`;
 
 export const AuthService = {
-    async logIn(logInDto: {login: string, password: string}): Promise<string | null> {
+    async logIn(logInDto: {login: string, password: string}): Promise<{id: string, fullName: string} | null> {
         try {
             const response = await axios.post(`${API_URL}/try-to-log-in`, logInDto);
-            return response.data.fullName;
+            return response.data;
         } catch (error: any) {
+            console.error("Error occured while tryin to log in:", error);
             return null;
         }
     }
