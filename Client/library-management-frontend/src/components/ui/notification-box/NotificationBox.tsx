0778378ad57a2b8926a902349187869589153007
ArtemFlorko <artom.florko@gmail.com>
2024-09-04 22:44:55 +0300

    style: fix formatting and remove extra lines

--- Client/library-management-frontend/src/components/ui/notification-box/NotificationBox.tsx
+++ Client/library-management-frontend/src/components/ui/notification-box/NotificationBox.tsx
@@ -9,11 +9,11 @@ interface NotificationBoxProps {
 const NotificationBox: React.FC<NotificationBoxProps> = ({ visible, mainText, subText }) => {
     if (!visible) return <></>;
 
-    return(
-    <div className="notification-box text-center" role="alert">
-       <h1 className="main-text pb-2">{mainText}</h1>
-       <h3>{subText}</h3>
-    </div>
+    return (
+        <div className="notification-box text-center" role="alert">
+            <h1 className="main-text pb-2">{mainText}</h1>
+            <h3>{subText}</h3>
+        </div>
     )
 }
 
